8107bd28d8d561481655b152ba8a6a7ecdabf9c8
Johnny Dunn <johnnyddunn@gmail.com>
2016-04-04 20:58:33 -0500

    Added some links to empty HTML

---
+++ portfolio/js/app.js
@@ -0,0 +1,8 @@
+
+$(document).foundation();
+$(".off-canvas-submenu").hide();
+$(".off-canvas-submenu-call").click(function() {
+	var icon = $(this).parent().next(".off-canvas-submenu").is(':visible') ? '+' : '-';
+	$(this).parent().next(".off-canvas-submenu").slideToggle('fast');
+	$(this).find("span").text(icon);
+});
