b4595a3338c1546c22199c19a3600ea4b216d63f
Johnny Dunn <johnnyddunn@gmail.com>
2016-04-11 11:52:10 -0500

    Added refresh every minute

--- who-got-hacked-on-twitter/js/sketch.js
+++ who-got-hacked-on-twitter/js/sketch.js
@@ -7,11 +7,13 @@ function message(){
 
 function setup(){
 	console.log("yea");
-	
+	setTimeout(reload,60000);
     setTimeout(message,2000);
 }
 
-
+function reload () {
+	location.reload();
+}
 
 function draw () {
 	$("body").css("overflow", "hidden");
