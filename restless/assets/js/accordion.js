29592a39d912419ac0d10c1765aca0b9646657ee
Johnny Dunn <johnnyddunn@gmail.com>
2020-03-30 12:59:22 -0400

    make first panel active by default

--- restless/assets/js/accordion.js
+++ restless/assets/js/accordion.js
@@ -2,6 +2,18 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
+  if (i == 0) {
+    // Make first panel active by default
+    alert(acc[i].classList);
+    acc[i].classList.toggle("active");
+    alert(acc[i].classList);
+    var panel = acc[i].nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
@@ -9,6 +21,6 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
 }
