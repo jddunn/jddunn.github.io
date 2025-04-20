54d753e138844e6e59455b32662485ae7c2c993d
Johnny Dunn <johnnyfived@protonmail.com>
2025-04-20 16:52:12 -0700

    fix: fix right constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,5 +7,5 @@ export const HOME_OG_IMAGE_URL =
   'https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg';
 
 // Get environment variables with fallbacks
-export const GH = process.env.GH_API_TOKEN || '';
+export const GH_API_TOKEN = process.env.GH_API_TOKEN || '';
 export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID || '';
